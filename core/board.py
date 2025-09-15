d95a29fc3d278cd685e2198f63565712287cbd71
Maria Eva Modarelli <modarellimariaeva@gmail.com>
2025-09-14 22:02:46 -0300

    siguiendo con clase board #29

--- core/board.py
+++ core/board.py
@@ -5,3 +5,17 @@ class Board:
 
     def get_points(self):
         return self.__points__
+    
+    def get_point(self, index):
+            
+            if isinstance(index, int) and 0 <= index < 24:
+                return self.__points__[index]
+            raise ValueError("El índice debe estar entre 0 y 23")
+
+    def set_point(self, index, value):
+
+        if (isinstance(index, int) and 0 <= index < 24
+                and isinstance(value, int) and value >= 0):
+            self.__points__[index] = value
+        else:
+            raise ValueError("index 0..23 y value entero >= 0")
